b2414d0b199de5de7afba8165152d434f0b9f496
Suborna589 <chakrobortis101@gmail.com>
2024-06-24 19:26:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-blue-500 text-center text-5xl mt-12 '>Vite + React</h1>
+  
+    </>
+  )
+}
+
+export default App
