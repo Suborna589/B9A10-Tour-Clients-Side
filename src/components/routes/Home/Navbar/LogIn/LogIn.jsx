b7fdb3a4f3f092ad88d8461bef0f06f9089871ae
Suborna589 <chakrobortis101@gmail.com>
2024-06-27 18:40:52 +0600

    login page design almost done

--- src/components/routes/Home/Navbar/LogIn/LogIn.jsx
+++ src/components/routes/Home/Navbar/LogIn/LogIn.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import login from '../../../../../../public/login.jpg'
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
     return (
-        <div>
-            <h1>This is login page </h1>
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content lg:flex-row gap-24"> 
+        {/* lg:flex-row-reverse">  */}
+
+        <div className="text-center lg:text-left">
+            <img className='shadow-2xl w-[560px] h-[440px] rounded-xl' src={login} alt="" />
+          
+          </div>
+         
+          <div className="card bg-[#aed6f1] w-[450px] h-[520px] shrink-0 shadow-2xl">
+            <form className="card-body "> 
+                
+              <div className="form-control"> 
+              <p className="text-center text-white font-bold text-3xl">Sign in Now</p>
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" placeholder="email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="password" className="input input-bordered" required />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+
+             <p className='flex gap-12 text-center ml-6 mt-3'>Create have an Account?<Link to='/signup' className='text-[#0000FF] font-bold '>SignUp</Link></p>
+            </form> 
+          
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/components/routes/Home/SpotCard/SpotCards.jsx
+++ src/components/routes/Home/SpotCard/SpotCards.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const SpotCards = ({tourists}) => {
-    const{tourists_spot_name,average_cost,totalVisitorsPerYear,travel_time,seasonality}=tourists;
+    const{image,tourists_spot_name,average_cost,totalVisitorsPerYear,travel_time,seasonality}=tourists;
     return (
         <div> 
-            <div className="card card-compact bg-base-100 w-96 shadow-xl">
+            <div className="card card-compact bg-base-100 w-full h-[680px] shadow-xl">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-      alt="Shoes" />
+       className="h-[600px]"src={image}alt="tour" />
   </figure>
-  <div className="card-body">
+  <div className="card-body  h-[550px]">
     <h2 className="card-title">{tourists_spot_name}</h2>
     <p>{average_cost}</p>  
     <p>{totalVisitorsPerYear}</p>
